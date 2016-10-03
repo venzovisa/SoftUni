d395bb66596a4f4f8bc424c8b2788a5223d4b55f
Venzo <nazobvane@gmail.com>
2016-10-03 17:26:43 +0300

    ObjectsAndClassesLab Excercises 1-5

---
+++ ObjectsAndClassesLab/DistanceBetweenPoints/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DistanceBetweenPoints
+{
+    class Point {         
+       public int x;
+       public int y;                
+    }
+
+    class Program
+    {
+        static double dist(Point p1, Point p2) {
+            double a, b, c;
+            a = p1.x - p2.x;
+            b = p1.y - p2.y;
+            c = Math.Sqrt(a * a + b * b);
+            return c;
+        }
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            Point p2 = new Point();            
+            int[] input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            p1.x = input[0];
+            p1.y = input[1];
+            input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            p2.x = input[0];
+            p2.y = input[1];
+            Console.WriteLine("{0:f3}", dist(p1, p2));
+        }
+    }
+}
---
+++ ObjectsAndClassesLab/RandomizeWords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+namespace RandomizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = Console.ReadLine().Split(' ').ToArray();
+            for (int i = 0; i < str.Length; i++)
+            {
+                Random rnd = new Random();
+                int randomIndex = rnd.Next(0, str.Length);
+                string tempIndex = str[i];    
+                str[i] = str[randomIndex];
+                str[randomIndex] = tempIndex;
+            }
+            for (int i = 0; i < str.Length; i++)
+            {
+                Console.WriteLine(str[i]);
+            }
+            
+        }
+    }
+}
