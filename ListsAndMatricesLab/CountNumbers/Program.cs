0944ae33bc5703a3120456d6451cb4610ec062c6
Venzo <nazobvane@gmail.com>
2016-10-03 17:49:07 +0300

    Lists And Matrices Lab First Commit

---
+++ ListsAndMatricesLab/CountNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CountNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Array.Sort(num);
+            int max = 1;
+            int prev = num[0];
+            for (int i = 1; i < num.Length; i++)
+            {
+                if (prev == num[i]) { max++; }
+                else {
+                    Console.WriteLine("{0} -> {1}", prev, max);
+                    prev = num[i];
+                    max = 1;
+                }
+            }
+            Console.WriteLine("{0} -> {1}", prev, max);
+        }
+    }
+}
---
+++ ListsAndMatricesLab/ListsAndMatricesLab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> num = Console.ReadLine().Split(' ').Select(Int32.Parse).ToList();
+            List<int> result = new List<int>();
+            foreach (int n in num) {
+                if (n > 0)
+                {
+                    result.Add(n);
+                }
+            }
+
+            result.Reverse();
+
+            if (result.Count == 0) {
+                Console.WriteLine("empty");
+            }
+            else {
+                foreach (int r in result)
+                {
+                    Console.Write(r + " ");
+                }
+            }            
+        }
+    }
+}
